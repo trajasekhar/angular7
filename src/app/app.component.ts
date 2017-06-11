10fc662c9e90df4d630004c316217e8165d2803a
trajasekhar <traja9581@gmail.com>
2017-06-11 13:39:29 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'welcome angular2';
+  data="raja";
 }
